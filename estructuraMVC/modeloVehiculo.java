8b16df2fe92c436e9e1925ed69b6940b40ea6540
Angel <armasangel193@gmail.com>
2024-08-20 14:17:37 -0600

    método constructor modeloVehiculo con sus getters y setters

--- estructuraMVC/modeloVehiculo.java
+++ estructuraMVC/modeloVehiculo.java
@@ -8,4 +8,33 @@ public class modeloVehiculo {
     private boolean disponible;
 
     // Métodos: Getters, Setters, actualizarDisponibilidad, calcularPrecio
+
+    //METODO CONSTRUCTOR
+    public modeloVehiculo(int id, String marca, String modelo, int año, String tipo, double precio, boolean disponible){
+        this.id = id;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.año = año;
+        this.tipo = tipo;
+        this.precio = precio;
+        this.disponible = disponible;
+    }
+
+    //GETTERS
+    public int getID(){return id;}
+    public String getMarca(){return marca;}
+    public String getModelo(){return modelo;}
+    public int getAño(){return año;}
+    public String getTipo(){return tipo;}
+    public double getPrecio(){return precio;}
+    public boolean getDisponible(){return disponible;}
+
+    //SETTERS
+    public void setID(int id){this.id = id;}
+    public void setMarca(String marca){this.marca = marca;}
+    public void setModelo(String modelo){this.modelo = modelo;}
+    public void setAño(int año){this.año = año;}
+    public void setTipo(String tipo){this.tipo = tipo;}
+    public void setPrecio(double precio){this.precio = precio;}
+    public void setDisponible(boolean disponible){this.disponible = disponible;}
 }

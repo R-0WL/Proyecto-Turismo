768adfe4d89106ad0b5ff97b5a3cb7f9adb9afe7
Angel <armasangel193@gmail.com>
2024-08-19 22:01:54 -0600

    Getters y Setters modelo usuario

--- estructuraMVC/modeloUsuario.java
+++ estructuraMVC/modeloUsuario.java
@@ -7,4 +7,26 @@ public class modeloUsuario {
 
     // Métodos: Getters, Setters, registrar, iniciarSesion, actualizarPerfil, cambiarContraseña
     
+    //METODO CONSTRUCTOR
+    public modeloUsuario ( int id, String nombre, String correo, String contraseña, String tipoUsuario){
+        this.id = id;
+        this.nombre = nombre;
+        this.correo = correo;
+        this.contraseña = contraseña;
+        this.tipoUsuario = tipoUsuario;
+    }
+
+    //GETTERS
+    public int getID(){return id;}
+    public String getNombre(){return nombre;}
+    public String getCorreo(){return correo;}
+    public String getContraseña(){return contraseña;}
+    public String getTipoUsuario(){return tipoUsuario;}
+
+    //SETTERS
+    public void setID(int id){this.id = id;}
+    public void setNombre(String nombre){this.nombre = nombre;}
+    public void setCorreo(String correo){this.correo = correo;}
+    public void setContraseña(String contraseña){this.contraseña = contraseña;}
+    public void setTipoUsuario(String tipoUsuario){this.tipoUsuario = tipoUsuario;}
 }

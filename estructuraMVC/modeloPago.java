b5ad8c17b65f331c6a748214e76d3eec5727e87a
Angel <armasangel193@gmail.com>
2024-08-21 21:18:09 -0600

    Metodo constructor, Getters y Setters de modeloPago

--- estructuraMVC/modeloPago.java
+++ estructuraMVC/modeloPago.java
@@ -1,13 +1,35 @@
 import java.util.Date;
 public class modeloPago {
     private int id;
-    private Reserva reserva;
+    private modeloReserva reserva;
     private double monto;
     private Date fechaPago;
     private String metodoPago;
 
     // Métodos: Getters, Setters, procesarPago, generarFactura
 
+    //METODO CONSTRUCTOR
+    public modeloPago (int id, modeloReserva reserva, double monto, Date fechaPago, String metodoPago){
+        this.id = id;
+        this.reserva = reserva;
+        this.monto = monto;
+        this.fechaPago = fechaPago;
+        this.metodoPago = metodoPago;
+    }
 
-    
+    //GETTERS
+    public int getID(){return id;}
+    public modeloReserva getReserva(){return reserva;}
+    public double getMonto(){return monto;}
+    public Date getFechaPago(){return fechaPago;}
+    public String getMetodoPago(){return metodoPago;}
+
+    //SETTERS
+    public void setID(int id){this. id = id;}
+    public void setReserva(modeloReserva reserva){this.reserva = reserva;}
+    public void setMonto(double monto){this.monto = monto;}
+    public void setFechaPago(Date fechaPago){this.fechaPago = fechaPago;}
+    public void setMetodoPago(String metodoPago){this.metodoPago = metodoPago;}
+
+
 }

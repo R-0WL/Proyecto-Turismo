70cd7e0b9a9dff8b7983999a536a24edd82c059c
Armasanel <167255055+Armasanel@users.noreply.github.com>
2024-09-19 21:49:43 -0600

    Update controladorPago.java

--- estructuraMVC/controladorPago.java
+++ estructuraMVC/controladorPago.java
@@ -1,12 +1,28 @@
 public class controladorPago {
-    private Pago pago;
+    private modeloPago pago;
     private vistaPago vistaPago;
 
+    public controladorPago(modeloPago pago, vistaPago vistaPago){
+        this.pago = pago;
+        this.vistaPago = vistaPago;
+    }
+
     public void procesarPago(int reservaId, String metodoPago) {
-        // Lógica para procesar el pago
+        if(reservaId <= 0 || metodoPago == null || metodoPago.isEmpty()){
+            vistaPago.mostrarError("El ID de la reserva y el metodo de pago son obligatorios.");
+            return;
+        }
+        modeloReserva reserva = pago.obtenerReservaporId(reservaId);
+        if(reserva == null){
+            vistaPago.mostrarError("No se encontró la reserva con ID" +  reservaId);
+            return;
+        }
+        modeloPago nuevoPago = new modeloPago(reservaId, reserva, reservaId, null, metodoPago);
+        pago.guardarPago(nuevoPago);
+        vistaPago.mostrarMensajeExito("Pago procesado exitosamente para la reserva ID: " + reservaId);
     }
     
     public void generarFactura(int pagoId) {
-        // Lógica para generar la factura del pago
+        
     }
 }

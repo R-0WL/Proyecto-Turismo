d9f08c659668eed1024a29bef8b951892fd2c18d
Armasanel <167255055+Armasanel@users.noreply.github.com>
2024-09-19 21:50:17 -0600

    Update controladorVehiculo.java

--- estructuraMVC/controladorVehiculo.java
+++ estructuraMVC/controladorVehiculo.java
@@ -1,12 +1,39 @@
+import java.util.List;
+
 public class controladorVehiculo {
-    private Vehiculo vehiculo;
+    private modeloVehiculo vehiculo;
     private vistaBusquedaVehiculos vistaBusquedaVehiculos;
 
+    public controladorVehiculo(modeloVehiculo vehiculo, vistaBusquedaVehiculos vistaBusquedaVehiculos){
+        this.vehiculo = vehiculo;
+        this.vistaBusquedaVehiculos = vistaBusquedaVehiculos;
+    }
+
     public void buscarVehiculos(String criteriosBusqueda) {
-        // Lógica para buscar vehículos
+        if(criteriosBusqueda == null || criteriosBusqueda.isEmpty()){
+            vistaBusquedaVehiculos.mostrarError("Por favor, ingrese todos los criterios de búsqueda.");
+            return;
+        }
+        List<modeloVehiculo> resultados = vehiculo.buscarVehiculos(criteriosBusqueda);
+        if(resultados.isEmpty()){
+            vistaBusquedaVehiculos.mostrarError("No se encontraron vehiculos disponibles.");
+        }
+        else {
+            vistaBusquedaVehiculos.mostrarResultadosBusqueda(resultados);
+        }
     }
     
     public void mostrarDetallesVehiculo(int vehiculoId) {
-        // Lógica para mostrar detalles de un vehículo específico
+        if(vehiculoId <= 0){
+            vistaBusquedaVehiculos.mostrarError("El ID del vehículo no es válido.");
+            return;
+        }
+        modeloVehiculo vehiculoEncontrado = vehiculo.obtenerVehiculoPorID(vehiculoId);
+        if(vehiculoEncontrado != null){
+            vistaBusquedaVehiculos.mostrarDetallesVehiculo(vehiculoEncontrado);
+        }
+        else {
+            vistaBusquedaVehiculos.mostrarError("No se encontraron vehículos disponibles.");
+        }
     }
 }

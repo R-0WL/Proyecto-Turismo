1d8cc1d0bc35a5f3811283ba54859fea4a19e863
Hecta18 <62799494+Hecta18@users.noreply.github.com>
2024-10-15 00:16:54 -0600

    mejora de controladorPago, leer comentario

--- Easyrents/src/main/java/com/easyrents/controllers/controladorPago.java
+++ Easyrents/src/main/java/com/easyrents/controllers/controladorPago.java
@@ -9,15 +9,20 @@ public class controladorPago {
 
     public void procesarPago(int reservaId, String metodoPago) {
         if(reservaId <= 0 || metodoPago == null || metodoPago.isEmpty()){
+            //mejor implementar devolver valores, que luego seran procesados en el main
+            //que devolver texto. En mi opinion, Hector
             vistaPago.mostrarError("El ID de la reserva y el metodo de pago son obligatorios.");
             return;
         }
         Reserva reserva = pago.obtenerReservaporId(reservaId);
         if(reserva == null){
+            //mejor implementar devolver valores, que luego seran procesados en el main
+            //que devolver texto. En mi opinion, Hector
             vistaPago.mostrarError("No se encontró la reserva con ID" +  reservaId);
             return;
         }
-        Pago nuevoPago = new Pago(reservaId, reserva, reservaId, null, metodoPago);
+        //obtencion de datos de la reserva y crear con ellos un pago
+        Pago nuevoPago = new Pago(reservaId, reserva, reserva.getMonto, reserva.getFechaPago, metodoPago);
         pago.guardarPago(nuevoPago);
         vistaPago.mostrarMensajeExito("Pago procesado exitosamente para la reserva ID: " + reservaId);
     }
@@ -25,12 +30,16 @@ public class controladorPago {
     public void generarFactura(int pagoId) {
         Pago pago = pago.obtenerPagoPorID(pagoId);
         if (pago == null){
-            //MOSTRAR MENSAJE DE ERROR (NO SE ENCONTRO EL PAGO CON ID: -----)
+            vistaPago.mostrarError("No se encontró el pago con ID " + pagoId);
             return;
         }
 
         //GENERAR FACTURA AQUÍ COMO UN TXT O ALGO
-
+        String factura = "Factura para el pago ID: " + pagoId + "\n" +
+                             "Reserva asociada: " + pago.getReserva().getId() + "\n" +
+                             "Método de pago: " + pago.getMetodoPago() + "\n" +
+                             "Monto: $" + pago.getMonto() + "\n" +
+                             "Fecha: " + pago.getFecha();
         //MENSAJE DE EXITO DE LA GENERACION DE LA FACTURA E IMPRESION DE LA MISMA   
     }
 }

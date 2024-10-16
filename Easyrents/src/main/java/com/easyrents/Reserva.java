190825886cf2b0447cc8049935f69525ce88dc72
Hecta18 <62799494+Hecta18@users.noreply.github.com>
2024-10-16 13:44:17 -0600

    nuevo atributo reserva, avance controladorPago

--- Easyrents/src/main/java/com/easyrents/Reserva.java
+++ Easyrents/src/main/java/com/easyrents/Reserva.java
@@ -7,15 +7,17 @@ public class Reserva {
     private Date fechaInicio;
     private Date fechaFin;
     private double total;
+    private double monto;
 
     //METODO CONSTRUCTOR
-    public Reserva(int id, Usuario usuario, Vehiculo vehiculo, Date fechaInicio, Date fechaFin, double total) {
+    public Reserva(int id, Usuario usuario, Vehiculo vehiculo, Date fechaInicio, Date fechaFin, double total, double monto) {
         this.id = id;
         this.usuario = usuario;
         this.vehiculo = vehiculo;
         this.fechaInicio = fechaInicio;
         this.fechaFin = fechaFin;
         this.total = total;
+        this.monto = monto;
     }
     
     //GETTERS
@@ -25,6 +27,7 @@ public class Reserva {
     public Date getFechaInicio() {return fechaInicio;}
     public Date getFechaFin() {return fechaFin;}
     public double getTotal() {return total;}
+    public double getMonto() {return monto;}
     
     //SETTERS
     public void setId(int id) {this.id = id;}    
@@ -33,12 +36,14 @@ public class Reserva {
     public void setFechaInicio(Date fechaInicio) {this.fechaInicio = fechaInicio;}
     public void setFechaFin(Date fechaFin) {this.fechaFin = fechaFin;}
     public void setTotal(double total) {this.total = total;}
+    public void setMonto(double monto) {this.monto = monto;}
+
     
     //TOSTRING
     @Override
     public String toString() {
         return "Reserva [id=" + id + ", usuario=" + usuario + ", vehiculo=" + vehiculo + ", fechaInicio=" + fechaInicio
-                + ", fechaFin=" + fechaFin + ", total=" + total + "]";
+                + ", fechaFin=" + fechaFin + ", total=" + total + ", monto=" + monto + "]";
     }
 }
 

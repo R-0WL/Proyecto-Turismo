5d58a3169fc4747f693b0d99d75910637b5fcabf
CrZt <carlozact@gmail.com>
2024-10-18 08:22:08 -0600

    SE añadio posible implementacion de equals

---
+++ OneDrive/Escritorio/Escritorio/UVG/CLASES/P.O.O/Proyecto-Turismo-main/Easyrents/src/main/java/com/easyrents/Reserva.java
@@ -0,0 +1,79 @@
+package com.easyrents;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class Reserva {
+    private int id;
+    private Usuario usuario;
+    private Vehiculo vehiculo;
+    private LocalDate fechaInicio;
+    private LocalDate fechaFin;
+    private double monto;
+
+    //METODO CONSTRUCTOR
+    public Reserva(int id, Usuario usuario, Vehiculo vehiculo, LocalDate fechaInicio, LocalDate fechaFin, double monto) {
+        this.id = id;
+        this.usuario = usuario;
+        this.vehiculo = vehiculo;
+        this.fechaInicio = fechaInicio;
+        //validacion
+        setFechaFin(fechaFin);
+        setMonto(monto);
+    }
+    
+    //GETTERS
+    public int getId() {return id;}
+    public Usuario getUsuario() {return usuario;}
+    public Vehiculo getVehiculo() {return vehiculo;}
+    public LocalDate getFechaInicio() {return fechaInicio;}
+    public LocalDate getFechaFin() {return fechaFin;}
+    public double getMonto() {return monto;}
+    
+    //SETTERS
+    public void setId(int id) {this.id = id;}    
+    public void setUsuario(Usuario usuario) {this.usuario = usuario;}
+    public void setVehiculo(Vehiculo vehiculo) {this.vehiculo = vehiculo;}
+    public void setFechaInicio(LocalDate fechaInicio) {this.fechaInicio = fechaInicio;}
+    //validar fecha final despues de inicial
+    public void setFechaFin(LocalDate fechaFin) {
+        if (fechaFin.isAfter(fechaInicio)) {
+            this.fechaFin = fechaFin;
+        } else {
+            throw new IllegalArgumentException("La fecha final debe ser posterior a la fecha de inicio");
+        }
+    }
+    //validar monto mayor 0
+    public void setMonto(double monto) {
+        if (monto > 0) {
+            this.monto = monto;
+        } else {
+            throw new IllegalArgumentException("El monto ingresado no es valido");
+        }
+    }
+    
+    //TOSTRING
+    @Override
+    public String toString() {
+        return "Reserva [id=" + id + ", usuario=" + usuario + ", vehiculo=" + vehiculo + ", fechaInicio=" + fechaInicio
+                + ", fechaFin=" + fechaFin + ", monto=" + monto + "]";
+    }
+
+    // @Override
+    // public boolean equals(Object o) {
+    //     if (this == o) return true;
+    //     if (o == null || getClass() != o.getClass()) return false;
+    //     Reserva reserva = (Reserva) o;
+    //     return idReserva == reserva.idReserva &&
+    //         Objects.equals(usuario, reserva.usuario) &&
+    //         Objects.equals(vehiculo, reserva.vehiculo) &&
+    //         Objects.equals(fechaInicio, reserva.fechaInicio) &&
+    //         Objects.equals(fechaFin, reserva.fechaFin);
+    // }
+
+    // @Override
+    // public int hashCode() {
+    //     return Objects.hash(idReserva, usuario, vehiculo, fechaInicio, fechaFin);
+    // }
+
+}
